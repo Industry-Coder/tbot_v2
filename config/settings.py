619d6805e17f654b43db18aca5b1513d8c7aa617
industry-coder <thomas.edukwei18@gmail.com>
2026-03-29 16:10:31 +0000

    Set production settings (DEBUG False, allowed hosts, static url)

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+DEBUG = False
+
+ALLOWED_HOSTS = ["165.227.165.47", "localhost", "127.0.0.1"]
+
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
