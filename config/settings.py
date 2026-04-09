6b837d2c19f2d5f4c43f0a13b7886373697acbe6
industry-coder <tom.edukwei@gmail.com>
2026-04-09 10:08:53 +0000

    30 minutes logout

--- config/settings.py
+++ config/settings.py
@@ -127,5 +127,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-SESSION_COOKIE_AGE = 300  # 5 minutes (300 seconds)
+SESSION_COOKIE_AGE = 1800  # 30 minutes (1800 seconds)
 SESSION_SAVE_EVERY_REQUEST = True
